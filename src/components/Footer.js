c2145aa281e8f3a2cb50778c9ff23d7039601dc6
Jinfan <kimthebestman@gmail.com>
2021-07-17 10:14:26 -0700

    Page structure, setting input form

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer className="footer">
+                ©2021 StarReact Tracker. All Rights Reserved. Website Made by Jinfan Zhang
+            </footer>
+        );
+    }
+}
+
+export default Footer;
